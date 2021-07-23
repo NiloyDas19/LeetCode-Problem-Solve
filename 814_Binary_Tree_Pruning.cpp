124f88568eec0fcb76d02db2a45922334f775d52
NiloyDas19 <niloydas5215@gmail.com>
2021-07-23 16:58:53 +0600

    814. Binary Tree Pruning
    It's a binary tree problem.

---
+++ 814_Binary_Tree_Pruning.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/binary-tree-pruning/
+
+/*
+ It's a binary tree problem.
+ Solved this problem using postorder traversal.
+ TC ---> O(n).
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root==NULL) return root;
+        root->left =  pruneTree(root->left);
+        root->right =  pruneTree(root->right);
+        
+        if(root->val==0&&root->left==NULL&&root->right==NULL){
+           root =  NULL;
+        }
+        
+        return root;
+    }
+};
