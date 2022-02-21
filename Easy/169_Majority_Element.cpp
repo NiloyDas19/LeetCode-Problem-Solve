9e97e4c111c0555ee379a226013a32b63fe487c9
Niloy Das <86280394+NiloyDas19@users.noreply.github.com>
2022-02-21 10:43:40 +0600

    Create 169_Majority_Element.cpp

---
+++ Easy/169_Majority_Element.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/majority-element/
+
+/*
+  It's a simple problem.But we need to know Boyer-Moore Majority Voting Algorithm
+  to solve this problem optimally.
+  TC-> O(n).
+  SC-> O(1).
+*/
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int Candidate = -1, vote = 0, cnt = 0, n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (vote == 0)
+                Candidate = nums[i];
+            (nums[i] == Candidate) ? vote++ : vote--;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == Candidate)
+                cnt++;
+            if (cnt > n / 2)
+                return Candidate;
+        }
+        return -1;
+    }
+};
