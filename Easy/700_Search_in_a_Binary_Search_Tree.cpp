5e93f94f300249826ffa39f3bc7a7a934e780c25
Niloy Das <86280394+NiloyDas19@users.noreply.github.com>
2022-04-14 10:11:40 +0600

    700. Search in a Binary Search Tree
    Easy

---
+++ Easy/700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+
+/*
+   Easy Binary Search Tree Problem
+   TC :  O(h) [ h is the height of the BST]
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!=NULL){
+            if(val==root->val) return root;
+            else if(val<root->val) root = root->left;
+            else root = root-> right;
+        }
+        return NULL;
+    }
+};
